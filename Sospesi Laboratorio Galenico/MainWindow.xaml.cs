978c898c68058f33408c9906adeaca76d5bc93b6
Gianluca Troncone <gtroncone@treelog.it>
2017-02-17 19:14:17 +0100

    ambreck 16/02/17

--- Sospesi Laboratorio Galenico/MainWindow.xaml.cs
+++ Sospesi Laboratorio Galenico/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace Sospesi_Laboratorio_Galenico
 {
@@ -23,6 +24,20 @@ namespace Sospesi_Laboratorio_Galenico
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            FbConnection connessione = new FbConnection("User=SYSDBA;Password=masterkey;Database="+Sospesi_Laboratorio_Galenico.Properties.Settings.Default.database +";DataSource="+Sospesi_Laboratorio_Galenico.Properties.Settings.Default.server+";Port = 3050; Dialect = 3; Charset = NONE; Role =; Connection lifetime = 15; Pooling = true;MinPoolSize = 0; MaxPoolSize = 50; Packet Size = 8192; ServerType = 0; ");
+            if (e.Key==Key.Enter)
+            {
+
+
+
+
+
+            }
         }
     }
 }
